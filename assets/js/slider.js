b120673e6d5a08054e354053ae8190f54ef01330
甲斐聖健 <>
2022-03-15 18:31:00 +0900

    top pc完成

---
+++ assets/js/slider.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+    arrows: true,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    speed: 500,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    fade: true,
+    cssEase: 'linear',
+    dots: true,
+});
